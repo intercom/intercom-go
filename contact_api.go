8da5f8418c24ba94bf5716b8552255499ba24d1e
Timothy Lim <tim.lim@intercom.io>
2017-01-12 20:09:41 +0800

    Update Contact API endpoint to correctly use POST

--- contact_api.go
+++ contact_api.go
@@ -54,7 +54,7 @@ func (api ContactAPI) create(contact *Contact) (Contact, error) {
 
 func (api ContactAPI) update(contact *Contact) (Contact, error) {
 	requestContact := api.buildRequestContact(contact)
-	return unmarshalToContact(api.httpClient.Patch("/contacts", &requestContact))
+	return unmarshalToContact(api.httpClient.Post("/contacts", &requestContact))
 }
 
 func (api ContactAPI) convert(contact *Contact, user *User) (User, error) {
